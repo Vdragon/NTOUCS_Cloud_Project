6b4c361fad04b7de1ed32d33221308ec01ea97e8
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-01-23 15:24:06 +0800

    葉方圓 - 改為只撈取固定範圍的物件

--- Service_control_GUI/src/test/HTMLGetterTest.java
+++ Service_control_GUI/src/test/HTMLGetterTest.java
@@ -21,14 +21,23 @@ public class HTMLGetterTest {
 		Solr tmp=new Solr();
 		tmp.defaultRun();*/
 		
-		
+		String urls="";
 		ArrayList checked_URL = new ArrayList();
-		
+		ArrayList<String> tmp=new ArrayList<String>();
+		for(int i=381786;i<382086;i++){
+			urls = "http://auto.8891.com.tw/usedauto-infos-"+i+".html";
+			tmp.add(urls);
+			checked_URL.add(tmp);
+			tmp=new ArrayList();
+		}
+		/*for(int i=0;i<checked_URL.size();i++){
+			System.out.println(((ArrayList)checked_URL.get(i)).get(0).toString());
+		}*/
 		
 		HTMLGetter test=new HTMLGetter(checked_URL); 
 		
-		//test.totalHtmlPase();
-		try
+		test.totalHtmlPase();
+		/*try
 		{
 			test.htmlPase("http://auto.8891.com.tw/usedauto-infos-381786.html",1);
 		}
@@ -36,10 +45,10 @@ public class HTMLGetterTest {
 		{
 			System.err.print("HTMLGetterTest.main  error!!\n");
 		}
-		
+		*/
 	//	test.create_URLS();
 		System.out.println("end getter!!");
-		System.err.print("台灣  "+test.get_urlLost()[0]+"\n東森  "+test.get_urlLost()[1]+"\n中信  "+test.get_urlLost()[2]);
+		//System.err.print("台灣  "+test.get_urlLost()[0]+"\n東森  "+test.get_urlLost()[1]+"\n中信  "+test.get_urlLost()[2]);
 		
 		
 	}
